e078ddb492dfc8731a63b23d6da1169e93fda53f
Paulo Dias <paulo.dias@substanciadigital.com>
2015-02-09 17:24:21 +0000

    Refactoring code

--- tests/prbdias/mbway/tests/Integration/CreateMerchantAliasIntegrationTest.php
+++ tests/prbdias/mbway/tests/Integration/CreateMerchantAliasIntegrationTest.php
@@ -24,7 +24,6 @@ class CreateMerchantAliasIntegrationTest extends IntegrationTestCase {
      */
     public function testCallMerchantAliasCreation()
     {
-        return true;
         /* Creating the WebService main object */
         $test = new CreateMerchantAlias();
         /* Creating the WebService message object */
